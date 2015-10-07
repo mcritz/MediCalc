ca59872c2aeec34a819ccda33547384d73726e3a
Critz <mcritz@critz.local>
2015-10-07 16:45:38 -0700

    stuff. I dunno

---
+++ Playgrounds/MedicalcSandbox.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func dynamicFunc(someFunc: Any, parameters: [Any]) -> Any {
+    if let
+    return "did not process \(someFunc)"
+}
